fc0373c8ed425e57962fd19cc38fc36856698c93
Kazenzi <jacintakazenzi99@gmail.com>
2023-11-14 06:51:29 +0300

    add task

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 
-export default function TodoForm() {
+export default function TodoForm({addTodo}) {
     const [value,setValue]=useState("")
     const handlesubmit =e=>{
         e.preventDefault();
+        addTodo(value)
+
+        setValue("")
     }
   return (
   <form className='TodoForm' onSubmit={handlesubmit}>
-    <input type="text" className='todo-input' placeholder='What is the task todaY?' 
+    <input type="text" className='todo-input' value={value} placeholder='What is the task todaY?' 
     onChange={(e)=>setValue(e.target.value)}/>
     <button type='submit' className='todo-btn'>Add Task</button>
   </form>
